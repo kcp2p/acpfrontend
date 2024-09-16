ceb21e793f80b628b8c5858f4b5382f3ed654565
Krittin Chuaiphikroh <krittin@42bangkok.com>
2024-09-17 01:43:13 +0900

    Add Button event to index
    Change-Id: I1bac300382c23023c61408e23a177767fa42ba6e
    Signed-off-by: Krittin Chuaiphikroh <krittin@42bangkok.com>

--- nextjs/pages/index.js
+++ nextjs/pages/index.js
@@ -4,38 +4,61 @@ import { Inter } from "next/font/google";
 import Grid from "@mui/material/Grid2";
 import { Box, Typography, Button, TextField } from "@mui/material"; // Use by LoginForm
 import useBearStore from "@/store/useBearStore";
+import { useState } from "react";
 
 function Home() {
+  const [inputText, setInputText] = useState("");
+  const [displayText, setDisplayText] = useState("");
+
+  const handleInputChange = (event) => {
+    setInputText(event.target.value);
+  };
+
+  const handleClick = () => {
+    setDisplayText(inputText);
+  }
+
   return (
     <Box textAlign="center" p={4} sx={{ bgcolor: '#FAD5A5' }}>
       <Typography variant="h4" gutterBottom>
         Index
       </Typography>
       <Grid container spacing={0.5}>
-        <Grid size={{ xs: 6, md: 4}}>
           <Button variant="contained" color="primary" href="/page1">
             Go to Page 1
           </Button>
-        </Grid>
-        <Grid size={{ xs: 6, md: 4}}>
           <Button variant="contained" color="primary" href="/page2">
             Go to Page 2
           </Button>
-        </Grid>
-        <Grid size={{ xs: 6, md: 4}}>
           <Button variant="contained" color="primary" href="/page3">
             Go to Page 3
           </Button>
-        </Grid>
-        <Grid size={{ xs: 6, md: 4}}>
-          <Button variant="contained" color="primary" href="/">
-            HOME
-          </Button>
-        </Grid>     
       </Grid>
 
-      <TextField id="filled-basic" label="Search" variant="filled" />
+      <Grid item xs={12} md={12}>
+        <TextField
+          id="filled-basic"
+          label="Search"
+          variant="filled"
+          value={inputText}
+          onChange={handleInputChange}
+        />
+      </Grid>
 
+      <Grid item xs={12} md={12}>
+        <Button variant="contained" color="primary" onClick={handleClick}>
+          Click
+        </Button>
+      </Grid>
+
+      {/* Display the text below */}
+      {displayText && (
+        <Grid item xs={12} md={12}>
+          <Typography variant="h6" gutterBottom>
+            You Search for: {displayText}
+          </Typography>
+        </Grid>
+      )}
     </Box>
   );
 }
