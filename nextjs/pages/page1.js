90e82f2b3fa971dc274e70251ea1ea2d1f516f8e
Krittin Chuaiphikroh <krittin@42bangkok.com>
2024-09-17 01:57:30 +0900

    Add Bar Chart
    Change-Id: I3763f044f7750fd47943bdf9354a8fc8c039d7bd
    Signed-off-by: Krittin Chuaiphikroh <krittin@42bangkok.com>

--- nextjs/pages/page1.js
+++ nextjs/pages/page1.js
@@ -1,15 +1,14 @@
 import React from "react";
 import { Box, Typography, Button } from "@mui/material";
+import { BarChart } from "@mui/x-charts/BarChart";
 
-export default function Test() {
+export default function BasicBars() {
   return (
-    <Box textAlign="center" p={4}>
-      <Typography variant="h1" gutterBottom>
-        Hello World, this is Page 1
-      </Typography>
-      <Button variant="contained" color="primary" href="/">
-        Go back to Home
-      </Button>
-    </Box>
+    <BarChart
+      xAxis={[{ scaleType: "band", data: ["Item Availability"]}]}
+      series={[{ data: [4] }, { data: [1] }, { data: [2] }, { data: [3] }]}
+      width={600}
+      height={300}
+    />
   );
 }
